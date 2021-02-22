d789bc9a05fd0e40dbcef92795dba27ec9e5c1c1
Trần Công Tuấn <77573308+smartinchang@users.noreply.github.com>
2021-02-22 14:53:00 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+/* code by webdevtrick ( https://webdevtrick.com ) */
+function enableEditMode() {
+  richTextField.document.designMode = "on";
+}
+function Edit(command) {
+  richTextField.document.execCommand(command, false, null);
+}
+function execVal(command, value) {
+  richTextField.document.execCommand(command, false, value);
+}
